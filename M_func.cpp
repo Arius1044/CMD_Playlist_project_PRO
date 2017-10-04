6f0520969817abb5e7744d660a7e366afce00830
Julia Stakhiv <31933411+JulliaS@users.noreply.github.com>
2017-10-04 23:30:47 +0300

    Update M_func.cpp

--- M_func.cpp
+++ M_func.cpp
@@ -2,10 +2,8 @@
 #include <string>;
 #include <fstream>
 void readfile() {
-	string way;
-	cout << "Enter the way to your file - ";
-	cin >> way;
-	ifstream f(way);
+	
+	ifstream f("help.txt");
 
 	int n = 0;
 	while (!f.eof()) {
