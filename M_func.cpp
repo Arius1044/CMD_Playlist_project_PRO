11dd2692990757faa0a690c6d3b73065e6a9c8f3
Vitaliy Nastosiak <31761508+Arius1044@users.noreply.github.com>
2017-10-05 01:26:10 +0300

    cd_add

--- M_func.cpp
+++ M_func.cpp
@@ -4,6 +4,7 @@
 #include <windows.h>
 #include <string>
 #include <iostream>
+
 using namespace std;
 
 void music_list(char* buffer)
@@ -30,4 +31,50 @@ void music_list(char* buffer)
 		FindClose(hand);
 	}
 	delete[] c_buffer;
+}
+
+void folder_list(char* buffer)
+{
+	cout << endl;
+
+	WIN32_FIND_DATA Folder_Data;
+	HANDLE hand;
+
+	char* c_buffer = new char[100];
+	strcpy(c_buffer, buffer);
+	strcat(c_buffer, "\\*");
+
+	hand = FindFirstFile(c_buffer, &Folder_Data);
+
+	if (hand != INVALID_HANDLE_VALUE)
+	{
+		do
+		{
+			if (Folder_Data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) cout << Folder_Data.cFileName << endl;
+			
+		} while (FindNextFile(hand, &Folder_Data) != 0);
+
+		FindClose(hand);
+	}
+	delete[] c_buffer;
+}
+
+void cd(char* buffer)
+{
+	int count = 0;
+	string nbuffer;
+	int size = strlen(buffer);
+	for (int i = size; i > 0; i--)
+	{
+		count++;
+		if (buffer[i] == '\\') 
+		{
+			if (buffer[i - 1] == ':') count--;
+			for (int j = 0; j <size - count + 1; j++) nbuffer+=buffer[j];
+			break;
+	    }
+	}
+
+	SetCurrentDirectory(nbuffer.c_str());
+	cout << endl;
 }
