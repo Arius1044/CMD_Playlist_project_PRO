6311bac9f03d0f983920fccb9f5836df80cc063d
Julia Stakhiv <31933411+JulliaS@users.noreply.github.com>
2017-10-04 23:12:42 +0300

    Update M_func.cpp

--- M_func.cpp
+++ M_func.cpp
@@ -1,1 +1,23 @@
-//h
+#include <iostream>;
+#include <string>;
+#include <fstream>
+void readfile() {
+	string way;
+	cout << "Enter the way to your file - ";
+	cin >> way;
+	ifstream f(way);
+
+	int n = 0;
+	while (!f.eof()) {
+		string definition;
+		f >>definition;
+		cout << definition <<" ";
+		if (definition == ";") {
+			cout << "\n";
+		}
+		n++;
+	}
+
+	f.close();
+
+}
