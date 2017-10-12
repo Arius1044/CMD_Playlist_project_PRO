f5293266743242d0731ed720a13cc83e82c08144
Vitaliy Nastosiak <31761508+Arius1044@users.noreply.github.com>
2017-10-13 02:06:00 +0300

    v 0.8

--- Playlist.cpp
+++ Playlist.cpp
@@ -1,12 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "MP3_INFO.h"
 #include "Playlist.h"
+#include "S_func.h"
 #include <windows.h>
 #include <string>
 #include <iostream>
 #include <fstream>
 
 using namespace std;
+
+
 
 Playlist::Playlist(string* paths, unsigned int count, string _name_)
 {
@@ -36,11 +39,11 @@ Playlist::Playlist(string* paths, unsigned int count, string _name_)
 	
 }
 
-void Playlist::Save()
+void Playlist::Save(string _path_)
 {
-	string _name_ = name;
-	_name_ += ".m3u";
 
+	string _name_ = _path_; 
+	_name_ += name + ".m3u";
 
 	ofstream m3u_file(_name_);
 	m3u_file << "#EXTM3U" << "\n";
@@ -54,7 +57,6 @@ void Playlist::Save()
 		}
 	}
 	m3u_file.close();
-	cout << "\n\n***Done***\n\n";
 }
 
 Playlist::Playlist( string _name_)
@@ -66,42 +68,64 @@ Playlist::Playlist( string _name_)
 Playlist::Playlist(string path, string _name_)
 {
 	count_of_songs = 0;
+	name = _name_;
 
 	ifstream m3u_file(path);
-	unsigned count = 0;
+	unsigned count = -1;
 
 	while (!m3u_file.eof())
 	{
-		string path;
+		char* _path = new char[1000];
 		char temp;
-		m3u_file >> temp;
-		if (temp=='\n') count++;
-		if ((count % 2 == 0) && (count != 0))
+		m3u_file.get(temp);
+
+		int counter = 0;
+
+		if (temp == '\n') count++;
+		if ((count == 1) && (!m3u_file.eof()))
 		{
 			char buf;
-			m3u_file >> buf;
-			for (int i = 0; buf != '\n'; i++) { path[i] = buf; m3u_file >> buf; } 
-			ifstream mp3_file(path, ios::binary || ios::in);
-			mp3_file.seekg(-128, ios::end);
-
-			mp3_file.read((char*)&Songs[count_of_songs].ID, sizeof(Songs[count_of_songs].ID));
-			mp3_file.read((char*)&Songs[count_of_songs].song, sizeof(Songs[count_of_songs].song));
-			mp3_file.read((char*)&Songs[count_of_songs].artist, sizeof(Songs[count_of_songs].artist));
-			mp3_file.read((char*)&Songs[count_of_songs].album, sizeof(Songs[count_of_songs].album));
-			mp3_file.read((char*)&Songs[count_of_songs].year, sizeof(Songs[count_of_songs].year));
-			mp3_file.read((char*)&Songs[count_of_songs].comment, sizeof(Songs[count_of_songs].comment));
-			mp3_file.read((char*)&Songs[count_of_songs].genre, sizeof(Songs[count_of_songs].genre));
-
-			strcpy(Songs[count_of_songs].path, path.c_str());
-			mp3_file.close();
-			count_of_songs++;
+			m3u_file.get(buf);
+			for (int i = 0; ((buf != '\n') && (!m3u_file.eof())); i++)
+			{ 
+				_path[i] = buf;
+			     m3u_file.get(buf); 
+				 counter++;
+				 if (buf == '\n')  count = 0;
+			}
+			if (!m3u_file.eof()) 
+			{
+				string c_path(_path, counter);
+				counter = 0;
+				Songs[count_of_songs] = read_mp3(c_path);
+				count_of_songs++;
+			}
 		}
+		delete[] _path;
 
 	}
+	
 
 	m3u_file.close();
 
 
+}
+
+
+
+void Playlist::get_info()
+{
+	for (int i = 0; i < count_of_songs; i++)
+	{
+		
+		cout << Songs[i].artist << "\t\t\t\t\t";
+		cout << Songs[i].song << "\t\t\t\t\t" << endl;
+	}
+}
+
+string Playlist::get_name()
+{
+	return name;
 }
 
 Playlist::~Playlist()
